af5b4b73e737da70b8af1ef36e8e451015a734e3
Привет <mer79@internet.ru>
2022-09-29 16:45:31 +0300

    домашнее задание

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 
     private final String name;
@@ -25,6 +27,24 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return name.equals(book.name) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги " + this.name + " Год " + this.year + " " + this.author;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-Author author1 = new Author( " Name1"," Surname1");
+Author author1 = new Author( " Генри Райдер"," Хаггард");
 Author author2 = new Author( " Name2"," Surname2");
 
-Book book1 = new Book("Title1", 2020, author1);
-Book book2 = new Book("Title2", 2022, author2);
 
-        System.out.println(" Автор: " + author1.getFullName());
-        System.out.println(" Автор: " + author2.getFullName());
+Book book1 = new Book("Дочь Монтесумы", 1956, author1 );
+Book book2 = new Book("Title2", 2022, author2);
+System.out.println(" Автор: " + author1.getFullName());
+System.out.println(" Автор: " + author2.getFullName());
+        System.out.println(author1);
+        System.out.println(author2);
+
+
 
         System.out.println(" Название книги: " + book1.getName() + ", год издания книги: " + book1.getYear() + ", автор книги: " + book1.getAuthor().getFullName());
         System.out.println(" Название книги: " + book2.getName() + ", год издания книги: " + book2.getYear() + ", автор книги: " + book2.getAuthor().getFullName());
-
+        System.out.println(book1);
+        System.out.println(book2);
 
     }
 }
